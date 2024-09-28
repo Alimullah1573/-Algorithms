d8a0fbb0e9ed947dd2ce91dcc63ab691e1031793
Alimullah <146616992+Alimullah1573@users.noreply.github.com>
2024-09-28 14:42:50 +0600

    Update GCD.py

--- 3.Mathe_Matical_Algorithm/LCM And GCD/GCD.py
+++ 3.Mathe_Matical_Algorithm/LCM And GCD/GCD.py
@@ -1,5 +1,23 @@
 #   G.C.D   :   Greatest Common Divisor
 
+def GCD(num1, num2):
+    while num2:
+        # a, b swap and a % b val
+        num1, num2 = num2, (num1 % num2)
+    return num1
+
+def LCM(num1, num2):
+    return ((num1 * num2) // GCD(num1, num2))
+# Driver Code
+if __name__ == '__main__':
+    # for _ in range(int(input().split()))
+        num1, num2 = map(int, input().split())
+
+        res_GCD = GCD(num1, num2)
+        res_LCM = LCM(num1, num2)
+        print(f'num1:->{num1} num2:->{num2}-> GCD:->',res_GCD)
+        print(f'num1:->{num1} num2:->{num2}-> LCM:->', res_LCM)
+
 
 
 # define a function
