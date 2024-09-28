a8da0910c145d477f2fc14218eb9dd8fc1a91eb4
Alimullah <146616992+Alimullah1573@users.noreply.github.com>
2024-09-28 14:41:14 +0600

    Update LCM.py

--- 3.Mathe_Matical_Algorithm/LCM And GCD/LCM.py
+++ 3.Mathe_Matical_Algorithm/LCM And GCD/LCM.py
@@ -1,10 +1,30 @@
 
 # L.C.M = least Common Multiple
 # GCD = Greatest Common Divisor
+def GCD(num1, num2):
+    while num2:
+        # a, b swap and a % b val
+        num1, num2 = num2, (num1 % num2)
+    return num1
+
+def LCM(num1, num2):
+    return ((num1 * num2) // GCD(num1, num2))
+
+
+# Driver Code
+if __name__ == '__main__':
+    # for _ in range(int(input().split()))
+        num1, num2 = map(int, input().split())
+
+        res_GCD = GCD(num1, num2)
+        res_LCM = LCM(num1, num2)
+        print(f'num1:->{num1} num2:->{num2}-> GCD:->',res_GCD)
+        print(f'num1:->{num1} num2:->{num2}-> LCM:->', res_LCM)
 
 
 
 
+# *************************************************************
 a = int(input('Enter a : '))
 b = int(input('Enter b : '))
 m = None
@@ -50,27 +70,6 @@ a, b = 15, 20
 print(f'{a} and {b}->GCD:->', gcd(a, b))
 print(f'{a} and {b}->LCM:->', lcm(a, b))
 
-
-#***************************************************
-def GCD(num1, num2):
-    while num2:
-        # a, b swap and a % b val
-        num1, num2 = num2, (num1 % num2)
-    return num1
-
-def LCM(num1, num2):
-    return ((num1 * num2) // GCD(num1, num2))
-
-
-# Driver Code
-if __name__ == '__main__':
-    # for _ in range(int(input().split()))
-        num1, num2 = map(int, input().split())
-
-        res_GCD = GCD(num1, num2)
-        res_LCM = LCM(num1, num2)
-        print(f'num1:->{num1} num2:->{num2}-> GCD:->',res_GCD)
-        print(f'num1:->{num1} num2:->{num2}-> LCM:->', res_LCM)
 
 
 
